563a2a5e32e4c8433362b467f4a752660b48ce54
pklimai <pklimai@users.noreply.github.com>
2016-08-03 14:39:36 +0300

    Create nova-vm-create.py

---
+++ openstack-python/nova-vm-create.py
@@ -0,0 +1,21 @@
+#! /usr/bin/python
+
+from novaclient import client
+
+nova = client.Client(version = 2, 
+        username = "admin", 
+        api_key = "contrail", 
+        project_id = "demo", 
+        auth_url = "http://10.10.10.230:5000/v2.0/")
+
+im = nova.images.find(name = "Core-Linux-Image")
+fl = nova.flavors.find(name = "Linux-Core")
+net_A = nova.networks.find(label = "VN-B")
+
+new_VM = nova.servers.create(name = "VN-B_VM-1", 
+        image = im, 
+        flavor = fl, 
+        nics = [{'net-id': net_A.id}] )
+
+print new_VM
+
