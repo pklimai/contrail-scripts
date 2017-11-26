759c0303e8128e2abe6744acd0c51c7f4f735bb2
pklimai <pklimai@users.noreply.github.com>
2017-11-26 16:38:21 +0300

    Create nova-vm-delete.py

---
+++ openstack-python/keystone_v3/nova-vm-delete.py
@@ -0,0 +1,23 @@
+#! /usr/bin/python
+
+import os
+
+from keystoneauth1 import identity, session
+from novaclient import client
+
+auth = identity.v3.Password(auth_url=os.environ['OS_AUTH_URL'],
+       username=os.environ['OS_USERNAME'],
+       password=os.environ['OS_PASSWORD'],
+       project_name="demo",
+       user_domain_id=os.environ['OS_USER_DOMAIN_NAME'],
+       project_domain_name=os.environ['OS_PROJECT_DOMAIN_NAME'])
+
+sess = session.Session(auth=auth, verify=False)
+
+nova = client.Client(version=2, session=sess)
+
+VMs = nova.servers.list()
+
+for VM in VMs:
+    print "Deleting %s" % VM.name
+    VM.delete()
